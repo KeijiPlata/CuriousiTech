424f0101f8f84a11dfae06279cc1927b5571a257
Zachary Tecson <johnzacharymereulbtecson@iskolarngbayan.pup.edu.ph>
2022-07-27 03:36:37 +0800

    Formmcq3.vb me.close from me.hide

--- Formmcq3.vb
+++ Formmcq3.vb
@@ -35,6 +35,6 @@ Public Class Formmcq3
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Me.Hide()
+        Me.Close()
     End Sub
 End Class
