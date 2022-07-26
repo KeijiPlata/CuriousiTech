cf7db14cd4cfd4a022ad4b4a4e09c5ce9487b71f
Lord Miacky Keiji Plata <lordmiackykeijieplata@iskolarngbayan.pup.edu.ph>
2022-07-26 17:57:41 +0800

    change font to agency fb formtf3

--- Formtf3.designer.vb
+++ Formtf3.designer.vb
@@ -50,10 +50,10 @@ Partial Class Formtf3
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("AngryBirds", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(38, 128)
+        Me.Label1.Font = New System.Drawing.Font("Agency FB", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(60, 126)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(223, 43)
+        Me.Label1.Size = New System.Drawing.Size(166, 45)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "True Or False"
         '
