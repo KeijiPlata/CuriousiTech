6b8eb7b9307ae1bdd58b086169aab46c3996b63a
John Christopher Go <johnchristophersgo@iskolarngbayan.pup.edu.ph>
2022-07-28 17:54:51 +0800

    renamed labels

--- StudenForm.designer.vb
+++ StudenForm.designer.vb
@@ -45,29 +45,29 @@ Partial Class StudenForm
         Me.Guna2Button7 = New Guna.UI2.WinForms.Guna2Button()
         Me.Guna2HtmlLabel5 = New Guna.UI2.WinForms.Guna2HtmlLabel()
         Me.Guna2HtmlLabel6 = New Guna.UI2.WinForms.Guna2HtmlLabel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
+        Me.top1score = New System.Windows.Forms.Label()
+        Me.top1name = New System.Windows.Forms.Label()
+        Me.top2name = New System.Windows.Forms.Label()
+        Me.top2score = New System.Windows.Forms.Label()
+        Me.top4name = New System.Windows.Forms.Label()
+        Me.top4score = New System.Windows.Forms.Label()
+        Me.top3name = New System.Windows.Forms.Label()
+        Me.top3score = New System.Windows.Forms.Label()
         Me.Label10 = New System.Windows.Forms.Label()
-        Me.Label11 = New System.Windows.Forms.Label()
-        Me.Label12 = New System.Windows.Forms.Label()
-        Me.Label13 = New System.Windows.Forms.Label()
-        Me.Label14 = New System.Windows.Forms.Label()
+        Me.top8name = New System.Windows.Forms.Label()
+        Me.top8score = New System.Windows.Forms.Label()
+        Me.top7name = New System.Windows.Forms.Label()
+        Me.top7score = New System.Windows.Forms.Label()
         Me.Label15 = New System.Windows.Forms.Label()
-        Me.Label16 = New System.Windows.Forms.Label()
-        Me.Label17 = New System.Windows.Forms.Label()
-        Me.Label18 = New System.Windows.Forms.Label()
-        Me.Label19 = New System.Windows.Forms.Label()
+        Me.top6name = New System.Windows.Forms.Label()
+        Me.top6score = New System.Windows.Forms.Label()
+        Me.top5name = New System.Windows.Forms.Label()
+        Me.top5score = New System.Windows.Forms.Label()
         Me.Label20 = New System.Windows.Forms.Label()
-        Me.Label21 = New System.Windows.Forms.Label()
-        Me.Label22 = New System.Windows.Forms.Label()
-        Me.Label23 = New System.Windows.Forms.Label()
-        Me.Label24 = New System.Windows.Forms.Label()
+        Me.top10name = New System.Windows.Forms.Label()
+        Me.top10score = New System.Windows.Forms.Label()
+        Me.top9name = New System.Windows.Forms.Label()
+        Me.top9score = New System.Windows.Forms.Label()
         CType(Me.Guna2PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Guna2PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -404,77 +404,77 @@ Partial Class StudenForm
         Me.Guna2HtmlLabel6.TabIndex = 29
         Me.Guna2HtmlLabel6.Text = "High Score:"
         '
-        'Label1
+        'top1score
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(1234, 207)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(54, 13)
-        Me.Label1.TabIndex = 31
-        Me.Label1.Text = "top1score"
+        Me.top1score.AutoSize = True
+        Me.top1score.Location = New System.Drawing.Point(1234, 207)
+        Me.top1score.Name = "top1score"
+        Me.top1score.Size = New System.Drawing.Size(54, 13)
+        Me.top1score.TabIndex = 31
+        Me.top1score.Text = "top1score"
         '
-        'Label2
+        'top1name
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(1296, 208)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(54, 13)
-        Me.Label2.TabIndex = 32
-        Me.Label2.Text = "top1name"
+        Me.top1name.AutoSize = True
+        Me.top1name.Location = New System.Drawing.Point(1296, 208)
+        Me.top1name.Name = "top1name"
+        Me.top1name.Size = New System.Drawing.Size(54, 13)
+        Me.top1name.TabIndex = 32
+        Me.top1name.Text = "top1name"
         '
-        'Label4
+        'top2name
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(1142, 248)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(54, 13)
-        Me.Label4.TabIndex = 34
-        Me.Label4.Text = "top2name"
+        Me.top2name.AutoSize = True
+        Me.top2name.Location = New System.Drawing.Point(1142, 248)
+        Me.top2name.Name = "top2name"
+        Me.top2name.Size = New System.Drawing.Size(54, 13)
+        Me.top2name.TabIndex = 34
+        Me.top2name.Text = "top2name"
         '
-        'Label5
+        'top2score
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(1080, 247)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(54, 13)
-        Me.Label5.TabIndex = 33
-        Me.Label5.Text = "top2score"
+        Me.top2score.AutoSize = True
+        Me.top2score.Location = New System.Drawing.Point(1080, 247)
+        Me.top2score.Name = "top2score"
+        Me.top2score.Size = New System.Drawing.Size(54, 13)
+        Me.top2score.TabIndex = 33
+        Me.top2score.Text = "top2score"
         '
-        'Label6
+        'top4name
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(1296, 353)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(54, 13)
-        Me.Label6.TabIndex = 39
-        Me.Label6.Text = "top4name"
+        Me.top4name.AutoSize = True
+        Me.top4name.Location = New System.Drawing.Point(1296, 353)
+        Me.top4name.Name = "top4name"
+        Me.top4name.Size = New System.Drawing.Size(54, 13)
+        Me.top4name.TabIndex = 39
+        Me.top4name.Text = "top4name"
         '
-        'Label7
+        'top4score
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(1234, 352)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(54, 13)
-        Me.Label7.TabIndex = 38
-        Me.Label7.Text = "top4score"
+        Me.top4score.AutoSize = True
+        Me.top4score.Location = New System.Drawing.Point(1234, 352)
+        Me.top4score.Name = "top4score"
+        Me.top4score.Size = New System.Drawing.Size(54, 13)
+        Me.top4score.TabIndex = 38
+        Me.top4score.Text = "top4score"
         '
-        'Label8
+        'top3name
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(1439, 256)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(54, 13)
-        Me.Label8.TabIndex = 37
-        Me.Label8.Text = "top3name"
+        Me.top3name.AutoSize = True
+        Me.top3name.Location = New System.Drawing.Point(1439, 256)
+        Me.top3name.Name = "top3name"
+        Me.top3name.Size = New System.Drawing.Size(54, 13)
+        Me.top3name.TabIndex = 37
+        Me.top3name.Text = "top3name"
         '
-        'Label9
+        'top3score
         '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(1377, 255)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(54, 13)
-        Me.Label9.TabIndex = 36
-        Me.Label9.Text = "top3score"
+        Me.top3score.AutoSize = True
+        Me.top3score.Location = New System.Drawing.Point(1377, 255)
+        Me.top3score.Name = "top3score"
+        Me.top3score.Size = New System.Drawing.Size(54, 13)
+        Me.top3score.TabIndex = 36
+        Me.top3score.Text = "top3score"
         '
         'Label10
         '
@@ -484,41 +484,41 @@ Partial Class StudenForm
         Me.Label10.Size = New System.Drawing.Size(0, 13)
         Me.Label10.TabIndex = 35
         '
-        'Label11
+        'top8name
         '
-        Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(1296, 609)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(54, 13)
-        Me.Label11.TabIndex = 49
-        Me.Label11.Text = "top8name"
+        Me.top8name.AutoSize = True
+        Me.top8name.Location = New System.Drawing.Point(1296, 609)
+        Me.top8name.Name = "top8name"
+        Me.top8name.Size = New System.Drawing.Size(54, 13)
+        Me.top8name.TabIndex = 49
+        Me.top8name.Text = "top8name"
         '
-        'Label12
+        'top8score
         '
-        Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(1234, 608)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(54, 13)
-        Me.Label12.TabIndex = 48
-        Me.Label12.Text = "top8score"
+        Me.top8score.AutoSize = True
+        Me.top8score.Location = New System.Drawing.Point(1234, 608)
+        Me.top8score.Name = "top8score"
+        Me.top8score.Size = New System.Drawing.Size(54, 13)
+        Me.top8score.TabIndex = 48
+        Me.top8score.Text = "top8score"
         '
-        'Label13
+        'top7name
         '
-        Me.Label13.AutoSize = True
-        Me.Label13.Location = New System.Drawing.Point(1296, 544)
-        Me.Label13.Name = "Label13"
-        Me.Label13.Size = New System.Drawing.Size(54, 13)
-        Me.Label13.TabIndex = 47
-        Me.Label13.Text = "top7name"
+        Me.top7name.AutoSize = True
+        Me.top7name.Location = New System.Drawing.Point(1296, 544)
+        Me.top7name.Name = "top7name"
+        Me.top7name.Size = New System.Drawing.Size(54, 13)
+        Me.top7name.TabIndex = 47
+        Me.top7name.Text = "top7name"
         '
-        'Label14
+        'top7score
         '
-        Me.Label14.AutoSize = True
-        Me.Label14.Location = New System.Drawing.Point(1234, 543)
-        Me.Label14.Name = "Label14"
-        Me.Label14.Size = New System.Drawing.Size(54, 13)
-        Me.Label14.TabIndex = 46
-        Me.Label14.Text = "top7score"
+        Me.top7score.AutoSize = True
+        Me.top7score.Location = New System.Drawing.Point(1234, 543)
+        Me.top7score.Name = "top7score"
+        Me.top7score.Size = New System.Drawing.Size(54, 13)
+        Me.top7score.TabIndex = 46
+        Me.top7score.Text = "top7score"
         '
         'Label15
         '
@@ -528,41 +528,41 @@ Partial Class StudenForm
         Me.Label15.Size = New System.Drawing.Size(0, 13)
         Me.Label15.TabIndex = 45
         '
-        'Label16
+        'top6name
         '
-        Me.Label16.AutoSize = True
-        Me.Label16.Location = New System.Drawing.Point(1296, 479)
-        Me.Label16.Name = "Label16"
-        Me.Label16.Size = New System.Drawing.Size(54, 13)
-        Me.Label16.TabIndex = 44
-        Me.Label16.Text = "top6name"
+        Me.top6name.AutoSize = True
+        Me.top6name.Location = New System.Drawing.Point(1296, 479)
+        Me.top6name.Name = "top6name"
+        Me.top6name.Size = New System.Drawing.Size(54, 13)
+        Me.top6name.TabIndex = 44
+        Me.top6name.Text = "top6name"
         '
-        'Label17
+        'top6score
         '
-        Me.Label17.AutoSize = True
-        Me.Label17.Location = New System.Drawing.Point(1234, 478)
-        Me.Label17.Name = "Label17"
-        Me.Label17.Size = New System.Drawing.Size(54, 13)
-        Me.Label17.TabIndex = 43
-        Me.Label17.Text = "top6score"
+        Me.top6score.AutoSize = True
+        Me.top6score.Location = New System.Drawing.Point(1234, 478)
+        Me.top6score.Name = "top6score"
+        Me.top6score.Size = New System.Drawing.Size(54, 13)
+        Me.top6score.TabIndex = 43
+        Me.top6score.Text = "top6score"
         '
-        'Label18
+        'top5name
         '
-        Me.Label18.AutoSize = True
-        Me.Label18.Location = New System.Drawing.Point(1296, 413)
-        Me.Label18.Name = "Label18"
-        Me.Label18.Size = New System.Drawing.Size(54, 13)
-        Me.Label18.TabIndex = 42
-        Me.Label18.Text = "top5name"
+        Me.top5name.AutoSize = True
+        Me.top5name.Location = New System.Drawing.Point(1296, 413)
+        Me.top5name.Name = "top5name"
+        Me.top5name.Size = New System.Drawing.Size(54, 13)
+        Me.top5name.TabIndex = 42
+        Me.top5name.Text = "top5name"
         '
-        'Label19
+        'top5score
         '
-        Me.Label19.AutoSize = True
-        Me.Label19.Location = New System.Drawing.Point(1234, 412)
-        Me.Label19.Name = "Label19"
-        Me.Label19.Size = New System.Drawing.Size(54, 13)
-        Me.Label19.TabIndex = 41
-        Me.Label19.Text = "top5score"
+        Me.top5score.AutoSize = True
+        Me.top5score.Location = New System.Drawing.Point(1234, 412)
+        Me.top5score.Name = "top5score"
+        Me.top5score.Size = New System.Drawing.Size(54, 13)
+        Me.top5score.TabIndex = 41
+        Me.top5score.Text = "top5score"
         '
         'Label20
         '
@@ -572,41 +572,41 @@ Partial Class StudenForm
         Me.Label20.Size = New System.Drawing.Size(0, 13)
         Me.Label20.TabIndex = 40
         '
-        'Label21
+        'top10name
         '
-        Me.Label21.AutoSize = True
-        Me.Label21.Location = New System.Drawing.Point(1296, 732)
-        Me.Label21.Name = "Label21"
-        Me.Label21.Size = New System.Drawing.Size(60, 13)
-        Me.Label21.TabIndex = 53
-        Me.Label21.Text = "top10name"
+        Me.top10name.AutoSize = True
+        Me.top10name.Location = New System.Drawing.Point(1296, 732)
+        Me.top10name.Name = "top10name"
+        Me.top10name.Size = New System.Drawing.Size(60, 13)
+        Me.top10name.TabIndex = 53
+        Me.top10name.Text = "top10name"
         '
-        'Label22
+        'top10score
         '
-        Me.Label22.AutoSize = True
-        Me.Label22.Location = New System.Drawing.Point(1230, 732)
-        Me.Label22.Name = "Label22"
-        Me.Label22.Size = New System.Drawing.Size(60, 13)
-        Me.Label22.TabIndex = 52
-        Me.Label22.Text = "top10score"
+        Me.top10score.AutoSize = True
+        Me.top10score.Location = New System.Drawing.Point(1230, 732)
+        Me.top10score.Name = "top10score"
+        Me.top10score.Size = New System.Drawing.Size(60, 13)
+        Me.top10score.TabIndex = 52
+        Me.top10score.Text = "top10score"
         '
-        'Label23
+        'top9name
         '
-        Me.Label23.AutoSize = True
-        Me.Label23.Location = New System.Drawing.Point(1296, 677)
-        Me.Label23.Name = "Label23"
-        Me.Label23.Size = New System.Drawing.Size(54, 13)
-        Me.Label23.TabIndex = 51
-        Me.Label23.Text = "top9name"
+        Me.top9name.AutoSize = True
+        Me.top9name.Location = New System.Drawing.Point(1296, 677)
+        Me.top9name.Name = "top9name"
+        Me.top9name.Size = New System.Drawing.Size(54, 13)
+        Me.top9name.TabIndex = 51
+        Me.top9name.Text = "top9name"
         '
-        'Label24
+        'top9score
         '
-        Me.Label24.AutoSize = True
-        Me.Label24.Location = New System.Drawing.Point(1234, 676)
-        Me.Label24.Name = "Label24"
-        Me.Label24.Size = New System.Drawing.Size(54, 13)
-        Me.Label24.TabIndex = 50
-        Me.Label24.Text = "top9score"
+        Me.top9score.AutoSize = True
+        Me.top9score.Location = New System.Drawing.Point(1234, 676)
+        Me.top9score.Name = "top9score"
+        Me.top9score.Size = New System.Drawing.Size(54, 13)
+        Me.top9score.TabIndex = 50
+        Me.top9score.Text = "top9score"
         '
         'StudenForm
         '
@@ -615,29 +615,29 @@ Partial Class StudenForm
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1540, 884)
-        Me.Controls.Add(Me.Label21)
-        Me.Controls.Add(Me.Label22)
-        Me.Controls.Add(Me.Label23)
-        Me.Controls.Add(Me.Label24)
-        Me.Controls.Add(Me.Label11)
-        Me.Controls.Add(Me.Label12)
-        Me.Controls.Add(Me.Label13)
-        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.top10name)
+        Me.Controls.Add(Me.top10score)
+        Me.Controls.Add(Me.top9name)
+        Me.Controls.Add(Me.top9score)
+        Me.Controls.Add(Me.top8name)
+        Me.Controls.Add(Me.top8score)
+        Me.Controls.Add(Me.top7name)
+        Me.Controls.Add(Me.top7score)
         Me.Controls.Add(Me.Label15)
-        Me.Controls.Add(Me.Label16)
-        Me.Controls.Add(Me.Label17)
-        Me.Controls.Add(Me.Label18)
-        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.top6name)
+        Me.Controls.Add(Me.top6score)
+        Me.Controls.Add(Me.top5name)
+        Me.Controls.Add(Me.top5score)
         Me.Controls.Add(Me.Label20)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.top4name)
+        Me.Controls.Add(Me.top4score)
+        Me.Controls.Add(Me.top3name)
+        Me.Controls.Add(Me.top3score)
         Me.Controls.Add(Me.Label10)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.top2name)
+        Me.Controls.Add(Me.top2score)
+        Me.Controls.Add(Me.top1name)
+        Me.Controls.Add(Me.top1score)
         Me.Controls.Add(Me.Guna2HtmlLabel5)
         Me.Controls.Add(Me.Guna2HtmlLabel6)
         Me.Controls.Add(Me.Guna2PictureBox4)
@@ -691,27 +691,27 @@ Partial Class StudenForm
     Friend WithEvents Guna2HtmlLabel6 As Guna.UI2.WinForms.Guna2HtmlLabel
     Friend WithEvents Guna2PictureBox4 As Guna.UI2.WinForms.Guna2PictureBox
     Friend WithEvents Guna2Button7 As Guna.UI2.WinForms.Guna2Button
-    Friend WithEvents Label11 As Label
-    Friend WithEvents Label12 As Label
-    Friend WithEvents Label13 As Label
-    Friend WithEvents Label14 As Label
+    Friend WithEvents top8name As Label
+    Friend WithEvents top8score As Label
+    Friend WithEvents top7name As Label
+    Friend WithEvents top7score As Label
     Friend WithEvents Label15 As Label
-    Friend WithEvents Label16 As Label
-    Friend WithEvents Label17 As Label
-    Friend WithEvents Label18 As Label
-    Friend WithEvents Label19 As Label
+    Friend WithEvents top6name As Label
+    Friend WithEvents top6score As Label
+    Friend WithEvents top5name As Label
+    Friend WithEvents top5score As Label
     Friend WithEvents Label20 As Label
-    Friend WithEvents Label6 As Label
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label8 As Label
-    Friend WithEvents Label9 As Label
+    Friend WithEvents top4name As Label
+    Friend WithEvents top4score As Label
+    Friend WithEvents top3name As Label
+    Friend WithEvents top3score As Label
     Friend WithEvents Label10 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label21 As Label
-    Friend WithEvents Label22 As Label
-    Friend WithEvents Label23 As Label
-    Friend WithEvents Label24 As Label
+    Friend WithEvents top2name As Label
+    Friend WithEvents top2score As Label
+    Friend WithEvents top1name As Label
+    Friend WithEvents top1score As Label
+    Friend WithEvents top10name As Label
+    Friend WithEvents top10score As Label
+    Friend WithEvents top9name As Label
+    Friend WithEvents top9score As Label
 End Class
--- StudenForm.vb
+++ StudenForm.vb
@@ -5,6 +5,7 @@ Imports System.Data.OleDb.OleDbDataReader
 
 Public Class StudenForm
     Dim con As New OleDbConnection(My.Settings.loginConnectionString)
+    Dim con2 As New OleDb.OleDbConnection(My.Settings.leaderboardConnectionString)
     'CONSTRUCTOR
     Dim LastName As String
     Dim FirstName As String
@@ -13,6 +14,58 @@ Public Class StudenForm
     Dim StudentID As String
     Dim UserID As Long
     Dim Section As String
+
+    Public Sub showScore()
+        Dim scores(10) As Integer
+        Dim scores2(10) As Integer
+        Dim totalscores(10) As Integer
+        Dim names(10) As String
+        Dim sql As String
+        Dim cmd As New OleDb.OleDbCommand
+        Dim myreader As OleDbDataReader
+
+        con2.Open()
+        ' This will get the score from the database
+        sql = "Select TOP 10 * from leaderboardtruefalse Order By Score DESC"
+        cmd.Connection = con2
+        cmd.CommandText = sql
+
+        ' read
+        myreader = cmd.ExecuteReader
+
+        If myreader.HasRows Then
+            For i As Integer = 1 To 10 Step 1
+                myreader.Read()
+                scores(i) = myreader("Score")
+                names(i) = myreader("UserName")
+            Next
+        End If
+
+        con2.Close()
+
+        ' put the the data inside the label
+        top1score.Text = scores(1).ToString
+        top1name.Text = names(1).ToString
+        top2score.Text = scores(2).ToString
+        top2name.Text = names(2).ToString
+        top3score.Text = scores(3).ToString
+        top3name.Text = names(3).ToString
+        top4score.Text = scores(4).ToString
+        top4name.Text = names(4).ToString
+        top5score.Text = scores(5).ToString
+        top5name.Text = names(5).ToString
+        top6score.Text = scores(6).ToString
+        top6name.Text = names(6).ToString
+        top7score.Text = scores(7).ToString
+        top7name.Text = names(7).ToString
+        top8score.Text = scores(8).ToString
+        top8name.Text = names(8).ToString
+        top9score.Text = scores(9).ToString
+        top9name.Text = names(9).ToString
+        top10score.Text = scores(10).ToString
+        top10name.Text = names(10).ToString
+
+    End Sub
     Public Sub New(ByVal varLastName As String, ByVal varFirstName As String, ByVal varMiddleName As String, ByVal varUserName As String, ByVal varStudentID As String, ByVal varUserID As Long)
         InitializeComponent()
         LastName = varLastName
@@ -105,7 +158,11 @@ Public Class StudenForm
         sectionForm.Show()
     End Sub
 
-    Private Sub Label18_Click(sender As Object, e As EventArgs) Handles Label18.Click
+    Private Sub Label18_Click(sender As Object, e As EventArgs) Handles top5name.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles top1score.Click
 
     End Sub
 End Class
