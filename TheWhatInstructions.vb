beae7bac9ff1b332419a6586dc3db7594f158297
Lord Miacky Keiji Plata <lordmiackykeijieplata@iskolarngbayan.pup.edu.ph>
2022-07-29 19:48:54 +0800

    * import thewhat instructions

---
+++ TheWhatInstructions.vb
@@ -0,0 +1,7 @@
+﻿Public Class TheWhatInstructions
+    Private Sub Guna2PictureBox2_Click(sender As Object, e As EventArgs) Handles Guna2PictureBox2.Click
+
+        Me.Close()
+
+    End Sub
+End Class
