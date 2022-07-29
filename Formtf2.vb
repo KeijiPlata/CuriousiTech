67943bacd8147bc62769c2bb36a4fa7746ce5154
Zachary Tecson <johnzacharymereulbtecson@iskolarngbayan.pup.edu.ph>
2022-07-29 19:13:39 +0800

    gameoverForm lilpat Success

--- Formtf2.vb
+++ Formtf2.vb
@@ -1,70 +1,45 @@
-﻿' imports
-Imports System.Data
-Imports System.Data.OleDb
-Public Class Formtf2
+﻿Public Class Formtf2
     Public highscore2 As Integer
     Public newhighscore As Integer
     Public displayScore As Integer
-    Dim con2 As New OleDb.OleDbConnection(My.Settings.leaderboardConnectionString)
-    Dim Username As String = LogInForm.getUserNamee.ToString
-    Dim highscore As Integer
-
-    Private Function Highest()
-        Dim sql As String
-        Dim cmd As New OleDb.OleDbCommand
-        Dim myreader As OleDbDataReader
-
-        ' Open connection between database
-        con2.Open()
-        ' This will get the highscore from the database
-        sql = "Select * from leaderboardtruefalse where UserName = '" & Username.ToString & "'"
-        cmd.Connection = con2
-        cmd.CommandText = sql
-
-        ' read
-        myreader = cmd.ExecuteReader
-        myreader.Read()
-
-        ' convert to int to compare 
-        highscore = Convert.ToInt32(myreader("Score"))
-        con2.Close()
-
-
-        Return Nothing
-    End Function
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' get the database score
-        Highest()
+
+        Formtf1.PictureBox1.Size = New Size(1645, 962)
+        Formtf1.PictureBox1.Location = New Point(-59, 0)
+        Formtf1.PictureBox1.Visible = True
+
 
         ' display the score
         Label2.Text = displayScore.ToString
 
         ' highscore2 always get the old highscore
-        Label5.Text = highscore.ToString
-        Label3.Text = "Score"
+        Label5.Text = highscore2.ToString
 
-        'If displayScore > highscore Then
-        '    Label3.Text = "New HighScore!"
+        If displayScore > highscore2 Then
+            Label3.Text = "New HighScore!"
 
-        '    ' This is the updated highscore
-        '    Label5.Text = newhighscore.ToString
-        'Else
-        '    Label3.Text = "Score"
-        'End If
+            ' This is the updated highscore
+            Label5.Text = newhighscore.ToString
+        Else
+            Label3.Text = "Score"
+        End If
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
         ' opens mainmenu
-        Dim StudentForm = New StudenForm(LogInForm.reader("TbUser", "LastName"), LogInForm.reader("TbUser", "FirstName"), LogInForm.reader("TbUser", "MiddleName"), LogInForm.reader("TbUser", "UserName"), LogInForm.reader("TbUser", "StudentID"), LogInForm.reader("TbUser", "UserID"))
-        StudentForm.Show()
+        Formtf3.highest() ' This will refresh the highscore
+        Formtf3.showScore() ' This will refresh the leaderboard
+        Formtf3.Show()
         Me.Close()
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+    Private Sub Guna2PictureBox2_Click(sender As Object, e As EventArgs) Handles Guna2PictureBox2.Click
         ' retry button, open again form1
+        Formtf1.PictureBox1.Visible = False
         Me.Close()
         Formtf1.Show()
-
     End Sub
 End Class
