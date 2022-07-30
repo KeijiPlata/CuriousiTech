34594218799515c0e15a4afe02f8df42c804bedb
John Christopher Go <johnchristophersgo@iskolarngbayan.pup.edu.ph>
2022-07-30 09:06:59 +0800

    fix bug close btn

--- quizgameEdit.vb
+++ quizgameEdit.vb
@@ -171,4 +171,8 @@ Public Class quizgameEdit
             bind_data()
         End If
     End Sub
+
+    Private Sub Guna2PictureBox2_Click(sender As Object, e As EventArgs) Handles Guna2PictureBox2.Click
+        Me.Close()
+    End Sub
 End Class
